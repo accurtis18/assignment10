af34b1b9a00f82ad0c756b2c1348e31481aceb5a
Alex <achriscurtis@gmail.com>
2020-06-22 23:42:00 -0500

    script to collect all info via cli

--- app.js
+++ app.js
@@ -9,3 +9,111 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+console.log("Hello, you're the manager of this team.")
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'teamMembers',
+            message: 'How many members are there on your team?'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your office number?'
+        }
+    ])
+    .then(async answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+        await createTeam(answers.teamMembers);
+    })
+
+    async function createTeam (members){
+        let i = 0;
+        while(i < members){
+            await inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'memberType',
+                        message: "Is the team member an Engineer or Intern?",
+                        choices: ['Engineer', 'Intern']
+                    },
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is their name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'What is their ID number?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'What is their email address?'
+                    }
+                ])
+                .then(async answers =>{
+                    if(answers.memberType === 'Engineer'){
+                        let github = await createEng();
+                        const e = new Engineer(answers.name, answers.id, answers.email, github);
+                        console.log(e);
+                    } else{
+                        let school = await createIntern();
+                        const intern = await new Intern(answers.name, answers.id, answers.email, school);
+                        console.log(intern);
+                    }
+                })
+                i++;
+        }
+    }
+
+    async function createEng(){
+        let git = ""
+        await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is their github username?'
+            }
+        ])
+        .then(answers =>{
+            git = answers.github;
+        })
+        return git;
+    }
+
+    async function createIntern(){
+        let school = "";
+        await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Which school do they attend?'
+            }
+        ])
+        .then(answers =>{
+            school = answers.school;
+        })
+        return school;
+    }
